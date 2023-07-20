6a3d57cedd38dc5117d1fa76039ba084d5360147
mnorval <mnorval@gmail.com>
2023-07-20 15:45:03 +0200

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FFT_SPECTRUM
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DrawBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawBox
+            // 
+            this.DrawBox.Location = new System.Drawing.Point(31, 13);
+            this.DrawBox.Name = "DrawBox";
+            this.DrawBox.Size = new System.Drawing.Size(363, 284);
+            this.DrawBox.TabIndex = 0;
+            this.DrawBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 309);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DrawBox);
+            this.Name = "Main";
+            this.Text = "FFT Naudio";
+            this.Load += new System.EventHandler(this.Do_On_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DrawBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DrawBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//*************************
+using NAudio;
+using NAudio.Wave;
+//*************************
+
+namespace FFT_SPECTRUM
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Do_On_Load(object sender, EventArgs e)
+        {
+            //******************************
+            //******************************
+
+            //******************************
+            //******************************
+
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //*********************************
+            Graphics graphic = this.DrawBox.CreateGraphics();
+            Pen selPen = new Pen(Color.Red);
+            Point coord1 = new Point(1, 1);
+            Point coord2 = new Point(100, 100);
+            graphic.DrawLine(selPen, coord1, coord2);
+            //*********************************
+            
+
+        }
+    }
+}
